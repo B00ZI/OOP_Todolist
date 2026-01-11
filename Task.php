fed64d5be10cafb9635478d0e4f0193f89e77865
B00ZI <iraamaneyahya@gmail.com>
2026-01-11 18:40:17 +0100

    Add Task class with methods to manage task state

---
+++ Task.php
@@ -0,0 +1,20 @@
+<?php
+class Task
+{
+
+    public function __construct(
+        private string $title,
+        private ?string $description = null,
+        private bool $done = false
+    ) {}
+
+    public function markDone(){
+        $this->done= true ; 
+    }
+    public function markUndone(){
+        $this ->done= false ; 
+    }
+    public function isDone(){
+        return  $this-> done ;
+    }
+}
